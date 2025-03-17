0a5d9caec48293507dd7a3dbd4abf02cf00710d5
Mbabazidinah <mbabazidinah90@gmail.com>
2025-03-17 20:59:19 +0400

    version 4

--- pyfun.py
+++ pyfun.py
@@ -3,14 +3,38 @@
 # print(len(name))
 # names = input("enter any names you want:").replace(" ","")
 # print(len(names))
-a = input("enter the first name A:")
-b = input("enter the second name B:")
-c = input("enter the third name C:")
-d = input("enter the forth name D:")
-e = input("enter the fifth name E:")
-a = a.replace(" ","")
-b = b.replace(" ","")
-c = c.replace(" ","")
-d = d.replace(" ","")
-e = e.replace(" ","")
-print("lengthof A=", len(a),"lengthof B=", len(b),"lengthof C=", len(c),"lengthof D=", len(d),"lengthof E=", len(e))
+# a = input("enter the first name A:")
+# b = input("enter the second name B:")
+# c = input("enter the third name C:")
+# d = input("enter the forth name D:")
+# e = input("enter the fifth name E:")
+# a = a.replace(" ","")
+# b = b.replace(" ","")
+# c = c.replace(" ","")
+# d = d.replace(" ","")
+# e = e.replace(" ","")
+# print("lengthof A=", len(a),"lengthof B=", len(b),"lengthof C=", len(c),"lengthof D=", len(d),"lengthof E=", len(e))
+arr1 = ['a', 'i', 'o', 'e', 'u']
+arr2 = ['k', 'j', 'p', 't', 'f']
+
+# Function to replace characters based on arr1 and arr2
+def kajipo(text):
+    result = []
+    for char in text:
+        if char in arr1:
+            index = arr1.index(char)
+            result.append(arr2[index])
+        elif char in arr2:
+            index = arr2.index(char)
+            result.append(arr1[index])
+        else:
+            result.append(char)
+    return ''.join(result)
+
+# Get user input
+user_input = input("Enter text: ")
+
+# Replace characters in the user input
+result = kajipo(user_input)
+
+print("Updated text:", result)
